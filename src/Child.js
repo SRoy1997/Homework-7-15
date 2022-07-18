87467c5f024b81f36b99e221ebf956f960dc763f
SRoy1997 <sarah@devpipeline.com>
2022-07-18 09:50:59 -0600

    Homework

---
+++ src/Child.js
@@ -0,0 +1,8 @@
+export default function Child(user) {
+  return (
+    <h3>
+      Hello {user.firstName + " " + user.lastName}! <br></br> Welcome to your
+      first React Page
+    </h3>
+  );
+}
